8b605dd9047f73c01d4451e920f88c32af266773
Josh Johnson <josh.johnson@leafyacre.com>
2018-08-10 23:29:20 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lsd-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'spartan-band-widgets';
+}
